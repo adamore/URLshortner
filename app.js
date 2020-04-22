5c0e3cae86dd7e1456e6f3da770faee151db3a3c
Anthony Damore <anthonym.damore@gmail.com>
2020-04-22 15:44:26 -0700

    changed app

--- app.js
+++ app.js
@@ -5,17 +5,5 @@ const PORT = process.env.PORT || 5000
 
 var url = require('url')
 var app = express()
-var server = http.Server(app)
 
-server.listen(PORT, function(request, response)
-{
-	var adr_ = request.url;
-	if(adr_ == "/")
-	{
-		app.use(express.static('public'));
-	}
-	else
-	{
-		app.use(express.static('public'));
-	}
-});
+app.listen(PORT);
