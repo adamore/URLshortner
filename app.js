3ae42fa39060add4c814c2e60f314fca12cecac2
Anthony Damore <anthonym.damore@gmail.com>
2020-04-25 21:17:59 -0700

    Added get and post

--- app.js
+++ app.js
@@ -1,9 +1,54 @@
-const http = require('http')
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+var http = require('http')
+var express = require('express')
+var bodyParser = require('body-parser')
+var PORT = process.env.PORT || 5000
+const { Client } = require('pg')
 
+connectionString = {
+	connectionString: process.env.DATABASE_URL,
+	ssl: true
+};
+
+var path = require('path')
 var url = require('url')
 var app = express()
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded( { extended : true }));
 
-app.listen(PORT);
+
+app.get('/', function(req, res) {
+	res.sendFile(path.join(__dirname + '/index.html'))
+});
+app.get('/index.js', function(req, res) {
+	res.sendFile(path.join(__dirname + '/index.js'))
+});
+app.get('/jquery.js', function(req, res) {
+	res.sendFile(path.join(__dirname + '/jquery.js'))
+});
+app.get('fdsafdsafdsafasd', function(req, res) {
+	var adr_ = req.path;
+	var longUrl = getLongUrl(adr_);
+	res.redirect(longUrl);
+});
+
+app.post("/submit", (req, res) => {
+	res.send(req.body.name);
+});
+app.listen(PORT)
+
+
+
+function getLongUrl(path_)
+{
+	return "https://www.google.com/";
+}
+
+function createURL(path_)
+{
+
+}
+
+function setDataBaseURL(ext_, path_)
+{
+	return true;
+}
--- index.js
+++ index.js
@@ -1,71 +1,27 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+document.getElementById("submitButton").addEventListener("click", formSubmit, false);
 
-function URLshortener(url_)
+function formSubmit()
 {
-	if(checkURL(url_))
-	{
-		return url_;
-	}
-	else
-	{
-		return "error";
-	}
+	$.ajax({
+        url:'/submit',
+        type:'post',
+        async: false,
+        data: $("input_url").value.serialize(),
+        error : function(jqXhr, textStatus, errorMessage) {
+        	window.alert("error");
+        },
+        success : function(response){
+        	window.alert(response);
+        }
+    });
+    return;
+}
 
-
-};
-
-
-function checkURL(url_)
-{
-	var irrelevant;
-	var request = $.ajax({
-		type: "GET",
-		url: url_,
-		data: irrelevant,
-		success: function(response){
-			return true;
-		},
-		error: function(response){
-			return false
-		}
+$(document).ready(function() {
+	document.getElementById("submitButton").click(function(){
 	});
-	return false;
-};
-
-function redirectUser(path_)
-{
-	var redirectURL_ = getURL(path_);
-	if(redirectURL_ == null)
-	{
-		window.location.replace(window.location.origin);
-	}
-	else
-	{
-		window.location.replace(redirectURL_);
-	}
-};
-
-function getURL(path_)
-{
-	if(path_ == "abc")
-	{
-		return "https://www.google.com/";
-	}
-	else
-	{
-		return null;
-	}
-
-};
+});
 
 
 
